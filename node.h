1a70bed2c2a0afa7ff6fb97ec22a70ff1b731501
lion137 <lion137@gmail.com>
2017-11-08 19:55:38 +0000

    Update node.h

--- node.h
+++ node.h
@@ -11,12 +11,11 @@ struct node {
    struct node * parent;
 };
 
-<<<<<<< HEAD
+
 struct node * init(long a); // constructor
 void node_print(struct node * a); // print key, payload
 void update_node_data(struct node * a); // updates the data field in a node  
-=======
-struct node * init(long a);
-void node_print(struct node * a);
->>>>>>> 7efb79bd4df8d62e26820d2801114f67f665d874
+
+
+
 #endif
